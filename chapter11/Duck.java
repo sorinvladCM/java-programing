682cc7a5d2fa8619ebc57bdde6ae4c72f6b8ecd1
sorinvladCM <pvlad@pentalog.com>
2021-09-28 15:54:41 +0300

    Delete chapter11 directory

--- chapter11/Duck.java
+++
@@ -1,9 +0,0 @@
-package exercises.chapter11;
-
-public class Duck extends Animal {
-
-    @Override
-    public void makeSound() {
-        System.out.println("quack quack");
-    }
-}
--- chapter11/Pig.java
+++
@@ -1,9 +0,0 @@
-package exercises.chapter11;
-
-public class Pig extends Animal {
-
-    @Override
-    public void makeSound() {
-        System.out.println("oink oink");
-    }
-}
